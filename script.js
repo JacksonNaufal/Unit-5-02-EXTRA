3990485101de462d8b4f1ec1101aeca35bc20047
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-16 17:13:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('button').addEventListener ('click', button)
+
+function button () {
+  document.getElementById('paragraph').innerHTML = 
+  "Look It Change, the Color also did, oh wow and the FONT SIZE!!"
+  document.getElementById('paragraph').style.color = 'orange'
+  document.getElementById('paragraph').style.fontSize= '500%'
+}
