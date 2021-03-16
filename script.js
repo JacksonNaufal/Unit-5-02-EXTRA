5672fe7e14e3d8f05e4fbf558b2a3747f3f29793
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-16 21:24:44 +0000

    Corrections

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 
 document.getElementById('button').addEventListener('click', button)
-document.getElementById('background').addEventListener('click', background)
+document.getElementById('background1').addEventListener('click', background1)
 document.getElementById('block').addEventListener('click', block)
-document.getElementById('background').addEventListener('click', background)
+document.getElementById('background1').addEventListener('click', background1)
 
 function button () {
   document.getElementById('paragraph').innerHTML =
   'Look It Change, the Color also did, oh wow and the FONT SIZE!!'
   document.getElementById('paragraph').style.color = 'teal'
   document.getElementById('paragraph').style.fontSize = '500%'
-  document.getElementById('background').style.backgroundColor = 'red'
+  document.getElementById('background1').style.backgroundColor = 'red'
 }
 
 function block () {
   document.getElementById('diamond').src =
 'Ocelots.png'
-  document.getElementById('background').style.backgroundColor = 'pink'
+  document.getElementById('background1').style.backgroundColor = 'pink'
 }
