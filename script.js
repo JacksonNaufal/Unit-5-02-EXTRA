ec234c34bf027b2889c667afafd0313b56522499
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-16 21:07:18 +0000

    Colors

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.getElementById('button').addEventListener('click', button)
+document.getElementById('background').addEventListener('click', background)
+document.getElementById('block').addEventListener('click', block)
 
 function button () {
   document.getElementById('paragraph').innerHTML =
   'Look It Change, the Color also did, oh wow and the FONT SIZE!!'
-  document.getElementById('paragraph').style.color = 'orange'
+  document.getElementById('paragraph').style.color = 'teal'
   document.getElementById('paragraph').style.fontSize = '500%'
+  document.getElementById ('background').style.backgroundColor = 'red'
+}
+
+function block () {
+  document.getElementById ('diamond').src = 
+'Ocelots.png'
+  document.getElementById ('background').style.backgroundColor =  'pink' 
 }
